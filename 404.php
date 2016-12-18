b241035a78c240ab9f78562f11c34f79042bc4ef
Marek Adamek <marek@Mareks-MacBook-Pro.local>
2016-12-18 17:58:36 +0100

    initial

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+	<div id="subhead_container">
+		
+		<div class="row">
+
+            <div class="twelve columns">
+                <h1>404 - Nic tu nie ma kolego</h1>
+            </div>
+			
+	    </div>
+	</div>
+
+<?php get_footer(); ?>
