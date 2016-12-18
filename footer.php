ad946990144c507d16888bd08570080f6085cc98
Marek Adamek <marek@Mareks-MacBook-Pro.local>
2016-12-18 19:05:03 +0100

    cleaning up

--- footer.php
+++ footer.php
@@ -3,9 +3,7 @@
 
 	    <!--footer container-->
 	    <div class="row">
-				
             <div class="twelve columns">
-
                 <div id="copyright">
                     <?php _e( '©', 'discover' ); ?> <?php echo date( 'Y' ); ?> <?php _e( 'Pull The Wire ~ wszystkie prawa zastrzeżone', 'discover' ); ?> |
                     <?php _e( 'Powered by', 'discover' ); ?> <a href="http://www.wordpress.org"><?php _e( 'WordPress', 'discover' ); ?></a>
